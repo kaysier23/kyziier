aedadfa4a1058f40786a35bd170b8ffacc9464f9
kaysier23 <60122621+kaysier23@users.noreply.github.com>
2020-03-20 16:36:54 +0800

    Update config.php

--- config.php
+++ config.php
@@ -10,7 +10,7 @@ $google_client->setClientId('910542507778-2hij5mionnfnmstepbh692b9dse51fs4.apps.
 
 $google_client->setClientSecret('hQ-03XkGGaxyPPXZCOqdt8Ae');
 
-$google_client->setRedirectUri('https://maja-etact.herokuapp.com/index.php');
+$google_client->setRedirectUri('https://ehzdf.herokuapp.com');
 
 $google_client->addScope('email');
 
